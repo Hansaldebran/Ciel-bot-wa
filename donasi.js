6c8fca40b52537f6f4a147fa83211d5e7286f935
Hansaldebran <76397974+Hansaldebran@users.noreply.github.com>
2020-12-21 12:13:04 +0700

    Update donasi.js

--- donasi.js
+++ donasi.js
@@ -1,32 +1,25 @@
-exports.donasi = (id, BotName, corohelp, tampilTanggal, tampilWaktu, instagram, telegram, youtube, kapanbotaktif, grupch1, grupch2) => {
-	return `🤜🤜{ *MENU DONASI ${BotName}* }🤛🤛
+ekspor . donasi  = ( id , BotName , corohelp , tampilTanggal , tampilWaktu , instagram , telegram , youtube , kapanbotaktif , grupch1 , grupch2 ) => {
+	return `{ *MENU DONASI ${BotName}* }
   
   
 TERIMA KASIH
 *${id.split("@s.whatsapp.net")[0]}*
 TELAH MEMBUKA MENU DONASI
-🐼🐼
 
 
-🗓*${tampilTanggal}* 🐼
-⏰*${tampilWaktu}* 🐼
+
+🗓*${tampilTanggal}* 
+⏰*${tampilWaktu}* 
 (Waktu Server)
 
 KALIAN BISA DONASI MENGGUNAKAN
    
-🛡 *PULSA*: 081271699856
-🛡 *OVO*: 081271699856
-🛡 *DANA* : 081271699866
-🛡 *GOPAY* : 081271699856
+🛡 *PULSA*: +6282158882713
 
-📺*IKLAN* *YOUTUBE REUZ*
+🛡 *DANA* : +6282158882713
 
 
-⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇⬇
-FOLLOW INSTAGRAM SAYA
-${instagram}
 
-👾TELEGRAM : ${telegram}
 
 `
 }
