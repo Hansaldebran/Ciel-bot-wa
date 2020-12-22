3df3445edaf7e4982b9dd90c1a554d2feb20c63c
Hansaldebran <76397974+Hansaldebran@users.noreply.github.com>
2020-12-22 21:44:31 +0700

    Update tambahan.js

--- tambahan.js
+++ tambahan.js
@@ -1,6 +1,6 @@
 module.exports = {
-	halo: "HALO JUGA ZEYENKK* \n\nketik *!menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
-	hai: "HAI JUGAA!*\n\n ketik *!menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
+	halo : "* *! menu * untuk menampilkan semua fitur yang ada \ r \ n \ r \ n \ n \ r \ n \ nketik ,
+	hai : "HAI JUGAA! * \ n \ n ketik *! menu * untuk menampilkan semua fitur yang ada \ r \ n \ r \ n \ n \ r" ,
 	ass: "WAALAIKUMSALAM*\n\n ketik *!menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
 	bro: "EUUUYYYY!!!* \n\nketik *!menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
 	p: "pa pe pa pe pap pap*\n\n ketik *!menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
