7102b9e5a69e22e90b3e59652f1e4dc56a378353
Hansaldebran <76397974+Hansaldebran@users.noreply.github.com>
2020-12-22 21:54:21 +0700

    Update tambahan.js

--- tambahan.js
+++ tambahan.js
@@ -1,9 +1,8 @@
 module.exports = {
-	halo : "* *! menu * untuk menampilkan semua fitur yang ada \ r \ n \ r \ n \ n \ r \ n \ nketik ,
-	hai : "HAI JUGAA! * \ n \ n ketik *! menu * untuk menampilkan semua fitur yang ada \ r \ n \ r \ n \ n \ r" ,
-	ass: "WAALAIKUMSALAM*\n\n ketik *!menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
-	bro: "EUUUYYYY!!!* \n\nketik *!menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
-	p: "pa pe pa pe pap pap*\n\n ketik *!menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
-	test: "*satu dua dicoba*\n\n ketik *!menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
-	sbr: "SABAR BOII LAGI DICARIIN",
-}
+	halo : "*hallo jugaa* \ n \ n ketik! menu * untuk menampilkan semua fitur yang ada \ r \ n \ r \ n \ n \ r \ n \ r" ,
+	hai : "*hi* \ n \ n ketik *! menu * untuk menampilkan semua fitur yang ada \ r \ n \ r \ n \ n \ r" ,
+	
+	bot: "hm?* \n\nketik *!menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
+	
+
+</s> </s> </s> </s> </s> </s> </s> </s> </s> </s> </s> </s> </s> </s> </s> </s> </s> </s> </s> </s> </s> </s> </s> </s> </s> </s> </s> orang </s>
